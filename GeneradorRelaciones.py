452314b96a7be6d3cbda7194e351f95e7c8bc07b
Diego A. Fern ndez <difernandez@uc.cl>
2015-11-02 22:40:53 -0400

    decode utf-8

--- GeneradorRelaciones.py
+++ GeneradorRelaciones.py
@@ -113,10 +113,10 @@ class GeneradorRelaciones(ProcesadorTexto.ProcesadorTexto):
         noticias = []
         doc1 = db.EmolModule.find()
         for document in doc1:
-           noticias.append(document['data'].encode("utf-8"))
+           noticias.append(document['data'].encode("utf-8").decode("utf-8", errors="ignore"))
         doc2 = db.LaTerceraModule.find()
         for document in doc2:
-        	noticias.append(document['data'].encode("utf-8"))
+        	noticias.append(document['data'].encode("utf-8").decode("utf-8", errors="ignore"))
         print(noticias)
         return noticias
 
